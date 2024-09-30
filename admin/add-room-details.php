b1b709add273f9b8accad8de50be400e08d64bdf
Supun Paniyanduwage <pdscharuka@gmail.com>
2024-09-30 21:54:55 +0530

    Update add-room-details.php

--- admin/add-room-details.php
+++ admin/add-room-details.php
@@ -9,7 +9,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Add Room Deatils</title>
+    <title>Add Room Deatils test</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
     <link href="assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
