ab6afc0e97ad111cf704f8f8b1881014ef62d0bd
Supun Paniyanduwage <pdscharuka@gmail.com>
2024-10-25 05:02:18 +0530

    Update accomodation.php

--- accomodation.php
+++ accomodation.php
@@ -52,7 +52,7 @@
                                 echo "<div class='col-lg-3 col-sm-6'>
                                     <div class='accomodation_item text-center'>
                                         <div class='hotel_img'>
-                                            <img src='admin/$row[2]' alt=''>
+                                            <img src='admin/assets/images/rooms/$row[2]' alt=''>
                                             <a href='#' class='btn theme_btn button_hover'>Book Now</a>
                                         </div>
                                         <a href='#'><h4 class='sec_h4'>$row[1]</h4></a>
