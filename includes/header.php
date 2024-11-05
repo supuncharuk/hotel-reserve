a397ac282a808a983884ef68fd56bb2ea450db1c
Supun Paniyanduwage <pdscharuka@gmail.com>
2024-11-05 20:41:53 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
         <nav class="navbar navbar-expand-lg navbar-light">
             <!-- Brand and toggle get grouped for better mobile display -->
             <a class="navbar-brand logo_h" href="index.php"><img src="assets/image/Logo.png" alt=""></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -17,7 +17,7 @@
                     <li class="nav-item"><a class="nav-link" href="accomodation.php">Accomodation</a></li>
                     <li class="nav-item"><a class="nav-link" href="gallery.php">Gallery</a></li>
                     <li class="nav-item submenu dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown" role="button" aria-expanded="false">Blog</a>
                         <ul class="dropdown-menu">
                             <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
                             <li class="nav-item"><a class="nav-link" href="blog-single.php">Blog Details</a></li>
