3735834150774e158f4ec448d2fd68591a20414f
Supun Paniyanduwage <pdscharuka@gmail.com>
2024-11-08 10:06:37 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -64,7 +64,7 @@
         </div>
         <div class="border_line"></div>
         <div class="row footer-bottom d-flex justify-content-between align-items-center">
-            <p class="col-lg-8 col-sm-12 footer-text m-0">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Design and Developed <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="#">PDS</a></p>
+            <p class="col-lg-8 col-sm-12 footer-text m-0">&copy; <script>document.write(new Date().getFullYear());</script> <a href="index.php">Royal Hotel</a> | All rights reserved | Design and Developed by <a href="#">Navodya</a></p>
             <div class="col-lg-4 col-sm-12 footer-social">
                 <a href="#"><i class="fa-brands fa-facebook"></i></a>
                 <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
